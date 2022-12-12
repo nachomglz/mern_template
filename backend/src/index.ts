edac4a8f4ede0247cb57c991f601062ae1b1cc54
nachomglz <nachopad3@gmail.com>
2022-12-12 19:38:12 +0100

    Added port from enviromental variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import logger from './utils/logger'
 
 const MONGO_URL: string =
   process.env.MONGO_URL ?? 'mongodb://localhost:27017/testtodo'
-const PORT: string | number = process.env.PORT ?? 3000
+const PORT: string | number = process.env.PORT ?? 3001
 
 async function main() {
   if (!MONGO_URL.length) return console.log('⚠️  ~ MONGO_URL not found!')
